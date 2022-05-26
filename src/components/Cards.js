34775d890dff7f9b7dd573a6023d09f973c37178
samar-alazzam <samarazzam354@yahoo.com>
2022-05-26 16:11:16 +0300

    123

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -8,6 +8,9 @@ export default function Cards(props){
     console.log("2, inside cards component ", props);
     return (
         <>
+          <container fluid className="main-container">
+            <div className="d-flex flex-wrap
+            justify-content-between w-75 ms-auto me-auto">
         {
             props.movies.map((movie)=>{
                 return (
@@ -18,6 +21,8 @@ export default function Cards(props){
     
             })
         }
+           </div>
+        </container>
     
         
     
--- src/components/ModalMovie.js
+++ src/components/ModalMovie.js
@@ -1,6 +1,7 @@
 
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form'
 export default function ModalMovie(props){
     return(
     <>
@@ -11,7 +12,22 @@ export default function ModalMovie(props){
         </Modal.Header>
         <Modal.Body>
         <img src={`https://image.tmdb.org/t/p/w400/${props.chosenMovie.poster_path}`} alt="Movie poster" />
+        {props.chosenMovie.overview}
         <br/>
+        <Form>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label>comment</Form.Label>
+    <Form.Control type="text" placeholder="Enter your comment" />
+    <Form.Text className="text-muted">
+      add your own comment
+    </Form.Text>
+  </Form.Group>
+
+  
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
         </Modal.Body>
         
         <Modal.Footer>
@@ -30,3 +46,6 @@ export default function ModalMovie(props){
     
 
 }
+
+
+
